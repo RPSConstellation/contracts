8a39cdca0f48405438016099896e693f0c97f887
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2023-11-07 17:09:43 +0530

    first commit

---
+++ contracts/CCIPTesting.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+import {LinkTokenInterface} from "./interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "./utils/Withdraw.sol";
+
+contract CCIPTesting is Withdraw, CCIPReceiver{
+
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
+    address immutable i_link;
+
+    bytes32 latestMessageId;
+    uint64 latestSourceChainSelector;
+    address latestSender;
+    string latestMessage;
+
+    event MessageReceived(
+        bytes32 latestMessageId,
+        uint64 latestSourceChainSelector,
+        address latestSender,
+        string latestMessage
+    );
+
+    event MessageSent(bytes32 messageId);
+
+    constructor(address router, address link) CCIPReceiver(router) {
+        i_link = link;
+        LinkTokenInterface(i_link).approve(router, type(uint256).max);
+    }
+
+    receive() external payable {}
+
+    function send(
+        uint64 destinationChainSelector,
+        address receiver,
+        string memory messageText,
+        PayFeesIn payFeesIn
+    ) external {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encode(messageText),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payFeesIn == PayFeesIn.LINK ? i_link : address(0)
+        });
+
+        uint256 fee = IRouterClient(i_router).getFee(
+            destinationChainSelector,
+            message
+        );
+
+        bytes32 messageId;
+
+        if (payFeesIn == PayFeesIn.LINK) {
+            // LinkTokenInterface(i_link).approve(i_router, fee);
+            messageId = IRouterClient(i_router).ccipSend(
+                destinationChainSelector,
+                message
+            );
+        } else {
+            messageId = IRouterClient(i_router).ccipSend{value: fee}(
+                destinationChainSelector,
+                message
+            );
+        }
+
+        emit MessageSent(messageId);
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        latestMessageId = message.messageId;
+        latestSourceChainSelector = message.sourceChainSelector;
+        latestSender = abi.decode(message.sender, (address));
+        latestMessage = abi.decode(message.data, (string));
+
+        emit MessageReceived(
+            latestMessageId,
+            latestSourceChainSelector,
+            latestSender,
+            latestMessage
+        );
+    }
+     function getLatestMessageDetails()
+        public
+        view
+        returns (bytes32, uint64, address, string memory)
+    {
+        return (
+            latestMessageId,
+            latestSourceChainSelector,
+            latestSender,
+            latestMessage
+        );
+    }
+}
